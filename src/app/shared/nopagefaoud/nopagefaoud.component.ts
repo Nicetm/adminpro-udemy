6d946c990a0fac62c94668a1c87ad9ccf879ac28
Pablo <pcsm.cs@gmail.com>
2018-12-21 01:01:09 -0300

    Primer Comit

---
+++ src/app/shared/nopagefaoud/nopagefaoud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefaoud',
+  templateUrl: './nopagefaoud.component.html',
+  styles: []
+})
+export class NopagefaoudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
